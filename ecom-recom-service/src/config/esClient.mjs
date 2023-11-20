94da85347b4c2381061e114dfa029332e19f850d
Jenny <jenny.maya186@gmail.com>
2023-11-20 14:30:03 +0700

    Updated the path

--- ecom-recom-service/src/config/esClient.mjs
+++ ecom-recom-service/src/config/esClient.mjs
@@ -2,9 +2,12 @@ import { Client } from "@elastic/elasticsearch";
 import { HttpConnection } from "@elastic/transport";
 import http from "http";
 import fs from 'fs';
+import path from "path";
 
 
 const agent = new http.Agent({ keepAlive: true });
+
+const certificatePath = path.join(__dirname, 'src', 'config', 'http_ca.crt');
 
 export const esClient = new Client({
   node: process.env.ES_CLIENT_URL || "https://127.0.0.1:9200",
@@ -13,7 +16,7 @@ export const esClient = new Client({
   },
   // caFingerprint: "47840e58e7dfe57f154e3c0bd2b70ffe5794e9a5d49b520ea27dd1db610cb2b7",
   tls: {
-    ca: fs.readFileSync('/Users/janny/Study/ecommerce-microservice/ecom-recom-service/src/config/http_ca.crt'),
+    ca: fs.readFileSync(certificatePath),
     rejectUnauthorized: false
   },
   log: "info",
